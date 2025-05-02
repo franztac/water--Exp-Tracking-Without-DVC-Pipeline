63a02332e7020f3fc3cad940a1c404882577906a
franztac <fratacc91@gmail.com>
2025-05-02 17:42:34 +0200

    Best Experiment with  n_estimators=1000

--- src/water_model.py
+++ src/water_model.py
@@ -5,7 +5,10 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from dvclive import Live
+import yaml
 
+
+n_estimators = yaml.safe_load(open("dvclive\params.yaml", "r"))["n_estimators"]
 
 data = pd.read_csv("data/water_potability.csv")
 train_data, test_data = train_test_split(data, test_size=0.20, random_state=42)
@@ -28,7 +31,7 @@ test_processed_data = fill_missing_with_median(test_data)
 X_train = train_processed_data.iloc[:, 0:-1].values
 y_train = train_processed_data.iloc[:, -1].values
 
-n_estimators = 500
+# n_estimators = 500
 
 clf = RandomForestClassifier(n_estimators=n_estimators)
 clf.fit(X_train, y_train)
